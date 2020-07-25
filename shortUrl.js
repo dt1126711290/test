0c248aafc9ef66ccab1c0528ddd2efaa2b27b2ec
大头 <dt1126711290@hotmail.com>
2020-07-25 12:31:13 +0800

    Update shortUrl.js

--- shortUrl.js
+++ shortUrl.js
@@ -1,15 +1,13 @@
-javascript:(function() {
-        const long_url = prompt('1')
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function(){
-            if( xhr.readyState === 4){
-                if( xhr.status >= 200 && xhr.status < 300 || xhr.status === 304){
-                    const data = xhr.responseText;
-                    const url = JSON.parse(data).ae_url
-                    alert(url)
-                }
-            }
-        };
-        xhr.open("get", 'https://api.uomg.com/api/long2dwz?dwzapi=suoim&url=' + long_url, true);
-        xhr.send(null);
-})();
+const long_url = prompt("输入短链接")
+const xhr = new XMLHttpRequest()
+xhr.onreadystatechange = function () {
+	if (xhr.readyState === 4) {
+		if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+			const data = xhr.responseText;
+			const url = JSON.parse(data).ae_url
+    	alert(url)
+  	}
+	}
+}
+xhr.open("get", 'https://api.uomg.com/api/long2dwz?dwzapi=suoim&url=' + long_url, true)
+xhr.send(null)
